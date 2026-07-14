4e7658cd36f18856c938b3df9c755f297ea07ae1
Keny Yoel Ramos Martínez <kecrafxd@gmail.com>
2026-07-13 19:41:30 -0600

    mmm

--- script.js
+++
@@ -1,9 +0,0 @@
-const fechas = document.querySelectorAll(".calendary__date");
-
-fechas.forEach((fecha) => {
-    fecha.addEventListener("click", () => {
-        const dia = fecha.getAttribute("data-date");
-
-        alert(dia);
-    });
-});
