335309e6df8f3b131461f675ad393d4a6ef667cc
Keny Yoel Ramos Martínez <kecrafxd@gmail.com>
2026-07-20 11:01:34 -0600

    Empezando a tener todo en un solo html

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ const btn_enviar = document.querySelector('#close')
 let eventos = [];
 
 
+
+
+//Segun el dia clickeado selecciona el dia automaticamente al momento de crear un evento
 for(let i = 0; i < day.length; i++){
     day[i].addEventListener('click',()=>{
         menu.showModal()
-        // console.log(menudayselect[(menudayselect.length) - i -1]) //No es buena practica PARA NADA; pero sirve :3Q4TG  87898,M   
+        // console.log(menudayselect[(menudayselect.length) - i -1]) //No es buena practica PARA NADA; pero sirve :3
         menudayselect[(menudayselect.length) - i -1].selected = true;
     })
 }
 
+//El formulario es enviado y dentro de un array se crea un evento
 formulario.addEventListener('submit', (form)=>{
     form.preventDefault();
     let evento = {
@@ -30,11 +34,14 @@ formulario.addEventListener('submit', (form)=>{
 
     eventos.push(evento)
 
+    //Usado como debug imprime los eventos existentes en el array 'eventos'
     for(let i = 0; i < eventos.length; i++){
         console.log(eventos[i])
     }
 
+    //Resetea el formulario al terminar de crear el evento
     formulario.reset();
 
+    //Cierra el menu
     menu.close()
 })
