1a38a46775d86f2911500db9cbdb1e6f324aaf58
Keny Yoel Ramos Martínez <kecrafxd@gmail.com>
2026-08-25 15:43:53 -0600

    Avance: 2027, 2028 añadidos

--- data_months.js
+++ data_months.js
@@ -1,86 +1,263 @@
 export let months = [
     {
-        name: "Enero",
-        start_position: 4,
-        first_day: 1,
-        days: 35,
-        data_year: 2026
-    },
-    {
-        name: "Febrero",
-        start_position: 0,
-        first_day: 1,
-        days: 28,
-        data_year: 2026
-    },
-    {
-        name: "Marzo",
-        start_position: 0,
-        first_day: 1,
-        days: 31,
-        data_year: 2026
-    },
-    {
-        name: "Abril",
-        start_position: 3,
-        first_day: 1,
-        days: 33,
-        data_year: 2026
-    },
-    {
-        name: "Mayo",
-        start_position: 5,
-        first_day: 1,
-        days: 36,
-        data_year: 2026
-    },
-    {
-        name: "Junio",
-        start_position: 1,
-        first_day: 1,
-        days: 31,
-        data_year: 2026
-    },
-    {
-        name: "Julio",
-        start_position: 3,
-        first_day: 1,
-        days: 34,
-        data_year: 2026
-    },
-    {
-        name: "Agosto",
-        start_position: 6,
-        first_day: 1,
-        days: 37,
-        data_year: 2026
-    },
-    {
-        name: "Septiembre",
-        start_position: 2,
-        first_day: 1,
-        days: 32,
-        data_year: 2026
-    },
-    {
-        name: "Octubre",
-        start_position: 4,
-        first_day: 1,
-        days: 35,
-        data_year: 2026
-    },
-    {
-        name: "Noviembre",
-        start_position: 0,
-        first_day: 1,
-        days: 30,
-        data_year: 2026
-    },
-    {
-        name: "Diciembre",
-        start_position: 2,
-        first_day: 1,
-        days: 33,
-        data_year: 2026
+        2026: [
+            {
+                name: "Enero",
+                start_position: 4,
+                first_day: 1,
+                days: 35,
+                data_year: 2026
+            },
+            {
+                name: "Febrero",
+                start_position: 0,
+                first_day: 1,
+                days: 28,
+                data_year: 2026
+            },
+            {
+                name: "Marzo",
+                start_position: 0,
+                first_day: 1,
+                days: 31,
+                data_year: 2026
+            },
+            {
+                name: "Abril",
+                start_position: 3,
+                first_day: 1,
+                days: 33,
+                data_year: 2026
+            },
+            {
+                name: "Mayo",
+                start_position: 5,
+                first_day: 1,
+                days: 36,
+                data_year: 2026
+            },
+            {
+                name: "Junio",
+                start_position: 1,
+                first_day: 1,
+                days: 31,
+                data_year: 2026
+            },
+            {
+                name: "Julio",
+                start_position: 3,
+                first_day: 1,
+                days: 34,
+                data_year: 2026
+            },
+            {
+                name: "Agosto",
+                start_position: 6,
+                first_day: 1,
+                days: 37,
+                data_year: 2026
+            },
+            {
+                name: "Septiembre",
+                start_position: 2,
+                first_day: 1,
+                days: 32,
+                data_year: 2026
+            },
+            {
+                name: "Octubre",
+                start_position: 4,
+                first_day: 1,
+                days: 35,
+                data_year: 2026
+            },
+            {
+                name: "Noviembre",
+                start_position: 0,
+                first_day: 1,
+                days: 30,
+                data_year: 2026
+            },
+            {
+                name: "Diciembre",
+                start_position: 2,
+                first_day: 1,
+                days: 33,
+                data_year: 2026
+            }
+        ],
+
+        2027: [
+            {
+                name: "Enero",
+                start_position: 5,
+                first_day: 1,
+                days: 36,
+                data_year: 2027
+            },
+            {
+                name: "Febrero",
+                start_position: 1,
+                first_day: 1,
+                days: 29,
+                data_year: 2027
+            },
+            {
+                name: "Marzo",
+                start_position: 1,
+                first_day: 1,
+                days: 32,
+                data_year: 2027
+            },
+            {
+                name: "Abril",
+                start_position: 4,
+                first_day: 1,
+                days: 34,
+                data_year: 2027
+            },
+            {
+                name: "Mayo",
+                start_position: 6,
+                first_day: 1,
+                days: 37,
+                data_year: 2027
+            },
+            {
+                name: "Junio",
+                start_position: 2,
+                first_day: 1,
+                days: 32,
+                data_year: 2027
+            },
+            {
+                name: "Julio",
+                start_position: 4,
+                first_day: 1,
+                days: 35,
+                data_year: 2027
+            },
+            {
+                name: "Agosto",
+                start_position: 0,
+                first_day: 1,
+                days: 31,
+                data_year: 2027
+            },
+            {
+                name: "Septiembre",
+                start_position: 3,
+                first_day: 1,
+                days: 33,
+                data_year: 2027
+            },
+            {
+                name: "Octubre",
+                start_position: 5,
+                first_day: 1,
+                days: 36,
+                data_year: 2027
+            },
+            {
+                name: "Noviembre",
+                start_position: 1,
+                first_day: 1,
+                days: 31,
+                data_year: 2027
+            },
+            {
+                name: "Diciembre",
+                start_position: 3,
+                first_day: 1,
+                days: 34,
+                data_year: 2027
+            }
+        ],
+        2028: [
+            {
+                name: "Enero",
+                start_position: 6,
+                first_day: 1,
+                days: 37,
+                data_year: 2028
+            },
+            {
+                name: "Febrero",
+                start_position: 2,
+                first_day: 1,
+                days: 31,
+                data_year: 2028
+            },
+            {
+                name: "Marzo",
+                start_position: 3,
+                first_day: 1,
+                days: 34,
+                data_year: 2028
+            },
+            {
+                name: "Abril",
+                start_position: 6,
+                first_day: 1,
+                days: 36,
+                data_year: 2028
+            },
+            {
+                name: "Mayo",
+                start_position: 1,
+                first_day: 1,
+                days: 32,
+                data_year: 2028
+            },
+            {
+                name: "Junio",
+                start_position: 4,
+                first_day: 1,
+                days: 34,
+                data_year: 2028
+            },
+            {
+                name: "Julio",
+                start_position: 6,
+                first_day: 1,
+                days: 37,
+                data_year: 2028
+            },
+            {
+                name: "Agosto",
+                start_position: 2,
+                first_day: 1,
+                days: 33,
+                data_year: 2028
+            },
+            {
+                name: "Septiembre",
+                start_position: 5,
+                first_day: 1,
+                days: 35,
+                data_year: 2028
+            },
+            {
+                name: "Octubre",
+                start_position: 0,
+                first_day: 1,
+                days: 31,
+                data_year: 2028
+            },
+            {
+                name: "Noviembre",
+                start_position: 3,
+                first_day: 1,
+                days: 33,
+                data_year: 2028
+            },
+            {
+                name: "Diciembre",
+                start_position: 5,
+                first_day: 1,
+                days: 36,
+                data_year: 2028
+            }
+        ]
     }
 ];
--- script.js
+++ script.js
@@ -23,13 +23,22 @@ const menuyear = document.querySelector("#añosel");
 const monthList = document.querySelector(".main-nav__month-list");
 const month_Cheked = monthList.getElementsByTagName("input");
 
+// Selector de anio
+const yearList = document.querySelector(".main-nav__year-list");
+const year_Cheked = yearList.getElementsByTagName("input");
+
 // Datos
 let eventos = [];
 let mes_seleccionado = hardware_date.getMonth();
+let year_seleccionado = hardware_date.getFullYear();
+
+// Debugsitos
+console.log(months[0][year_seleccionado][mes_seleccionado].name)
+
 function mostrarmodal(index) {
-    menu.showModal()
-    menudayselect[(menudayselect.length) - index].selected = true;
-    menumonth[(menumonth.length)-mes_seleccionado - 1].selected = true;
+  menu.showModal()
+  menudayselect[(menudayselect.length) - index].selected = true;
+  menumonth[(menumonth.length) - mes_seleccionado - 1].selected = true;
 }
 
 let addel = []
@@ -37,148 +46,241 @@ let addel = []
 
 //Seleccionar Mes segun la fecha del PC
 for (let i = 0; i < month_Cheked.length; i++) {
-    if (month_Cheked[i].value == mes_seleccionado) {
-        month_Cheked[i].checked = true
+  if (month_Cheked[i].value == mes_seleccionado) {
+    month_Cheked[i].checked = true
+  }
+}
+
+//Seleccionar anio segun fecha del PC
+for (let i = 0; i < year_Cheked.length; i++) {
+  if (year_Cheked[i].value == year_seleccionado - 2026) {
+    year_Cheked[i].checked = true
+  }
+}
+
+//Preparacion: Dejar todos vacios
+for (let i = 0; i < table_data.length; i++) {
+  table_data[i].textContent = "";
+}
+
+for (let i = 0; i < year_Cheked.length; i++) {
+  year_Cheked[i].addEventListener('input', () => {
+    mes_seleccionado = month_Cheked[i].value;
+
+    for (let i = 0; i < year_Cheked.length; i++) {
+      if (year_Cheked[i].checked == true) {
+        year_seleccionado = year_Cheked[i].value
+        console.log(year_Cheked)
+      }
     }
-}
 
-for (let i = 0; i < table_data.length; i++) {
-    table_data[i].textContent = "";
-}
+    //Limpieza para cada dia
+    for (let i = 0; i < table_data.length; i++) {
+      table_data[i].textContent = "";
+      day[i].removeEventListener("click", addel[i])
+      day[i].classList.remove('calendary__date--previous-month')
+    }
 
-for (let i = 0; i < month_Cheked.length; i++) {
-    month_Cheked[i].addEventListener('input', () => {
-        if (month_Cheked[i].checked == true) {
-            mes_seleccionado = month_Cheked[i].value;
+    //Limpeza de fw en caso de que exista
+    for (let i = 0; i < table_rows.length; i++) {
+      if (table_rows[i].classList.contains('fw')) {
+        table_rows[i].remove()
+      }
+    }
 
+    //Escribir el Nombre del mes seleccionado
+    calendary_header.textContent = months[0][year_seleccionado][mes_seleccionado].name + ' - ' + months[0][year_seleccionado][mes_seleccionado].data_year
 
+    //Empezar a dibujar
 
-            //Limpieza para cada dia
-            for (let i = 0; i < table_data.length; i++) {
-                table_data[i].textContent = "";
-                day[i].removeEventListener("click", addel[i])
-                day[i].classList.remove('calendary__date--previous-month')
-            }
+    //Si el mes inicia desde el indice 5 en adelante creara una nueva semana
+    if (months[0][year_seleccionado][mes_seleccionado].start_position >= 5) {
+      //fw is from 'five week
+      const fw = document.createElement('tr')
+      fw.classList.add('calendary__week-row')
+      fw.classList.add('fw')
 
-            //Limpeza de fw en caso de que exista
-            for (let i = 0; i < table_rows.length; i++) {
-                if (table_rows[i].classList.contains('fw')) {
-                    table_rows[i].remove()
-                }
-            }
-
-            //Escribir el Nombre del mes seleccionado
-            calendary_header.textContent = months[mes_seleccionado].name + ' - ' + months[mes_seleccionado].data_year
-
-            //Empezar a dibujar
-
-            //Si el mes inicia desde el indice 5 en adelante creara una nueva semana
-            if (months[mes_seleccionado].start_position >= 5) {
-                //fw is from 'five week
-                const fw = document.createElement('tr')
-                fw.classList.add('calendary__week-row')
-                fw.classList.add('fw')
-
-                for (let i = 0; i < 7; i++) {
-                    const fw_day = document.createElement('td')
-                    fw_day.classList.add('calendary__date')
-                    fw_day.classList.add('fw_day')
-                    fw_day.textContent = "";
-
-                    fw.appendChild(fw_day)
-                }
-
-                //Meter el five week en html
-                tbody.appendChild(fw);
-
-                //Creo que tiene sentido, cada vez se actualizan
-                day = document.querySelectorAll(".calendary__date");
-                table_data = tbody.querySelectorAll("td");
-                table_rows = tbody.querySelectorAll('tr')
-
-
-
-            }
-            let contador = 1;
-            for (let i = months[mes_seleccionado].start_position; i < months[mes_seleccionado].days; i++) {
-
-                table_data[i].textContent = contador
-                contador += 1
-
-                let index = i - months[mes_seleccionado].start_position + 1;
-
-                addel[i] = () => {
-                    mostrarmodal(index)
-                }
-                day[i].addEventListener('click', addel[i])
-            }
-
-            for (let i = 0; i < day.length; i++) {
-                if (day[i].textContent == "") {
-                    day[i].classList.add('calendary__date--previous-month')
-                }
-
-            }
-        }
-    })
-}
-
-//Solo cuando se carga la Pagina
-calendary_header.textContent = months[mes_seleccionado].name + ' - ' + months[mes_seleccionado].data_year
-
-for (let i = 0; i < table_data.length; i++) {
-    table_data[i].textContent = "";
-    day[i].removeEventListener("click", addel[i])
-    day[i].classList.remove('calendary__date--previous-month')
-}
-
-if (months[mes_seleccionado].start_position >= 5) {
-    //fw is from 'five week
-    const fw = document.createElement('tr')
-    fw.classList.add('calendary__week-row')
-    fw.classList.add('fw')
-
-    for (let i = 0; i < 7; i++) {
+      for (let i = 0; i < 7; i++) {
         const fw_day = document.createElement('td')
         fw_day.classList.add('calendary__date')
         fw_day.classList.add('fw_day')
         fw_day.textContent = "";
 
         fw.appendChild(fw_day)
+      }
+
+      //Meter el five week en html
+      tbody.appendChild(fw);
+
+      //Creo que tiene sentido, cada vez se actualizan
+      day = document.querySelectorAll(".calendary__date");
+      table_data = tbody.querySelectorAll("td");
+      table_rows = tbody.querySelectorAll('tr')
+
+
+
+    }
+    let contador = 1;
+    for (let i = months[0][year_seleccionado][mes_seleccionado].start_position; i < months[0][year_seleccionado][mes_seleccionado].days; i++) {
+
+      table_data[i].textContent = contador
+      contador += 1
+
+      let index = i - months[0][year_seleccionado][mes_seleccionado].start_position + 1;
+
+      addel[i] = () => {
+        mostrarmodal(index)
+      }
+      day[i].addEventListener('click', addel[i])
     }
 
-    //Meter el five week en html
-    tbody.appendChild(fw);
+    for (let i = 0; i < day.length; i++) {
+      if (day[i].textContent == "") {
+        day[i].classList.add('calendary__date--previous-month')
+      }
 
-    //Creo que tiene sentido, cada vez se actualizan
-    day = document.querySelectorAll(".calendary__date");
-    table_data = tbody.querySelectorAll("td");
-    table_rows = tbody.querySelectorAll('tr')
+    }
+  })
+}
+
+//Lo que se hace cada vez que se selecciona un mes
+for (let i = 0; i < month_Cheked.length; i++) {
+  month_Cheked[i].addEventListener('input', () => {
+    if (month_Cheked[i].checked == true) {
+      mes_seleccionado = month_Cheked[i].value;
+
+      for (let i = 0; i < year_Cheked.length; i++) {
+        if (year_Cheked[i].checked == true) {
+          year_seleccionado = year_Cheked[i].value
+          console.log(year_Cheked)
+        }
+      }
+
+      //Limpieza para cada dia
+      for (let i = 0; i < table_data.length; i++) {
+        table_data[i].textContent = "";
+        day[i].removeEventListener("click", addel[i])
+        day[i].classList.remove('calendary__date--previous-month')
+      }
+
+      //Limpeza de fw en caso de que exista
+      for (let i = 0; i < table_rows.length; i++) {
+        if (table_rows[i].classList.contains('fw')) {
+          table_rows[i].remove()
+        }
+      }
+
+      //Escribir el Nombre del mes seleccionado
+      calendary_header.textContent = months[0][year_seleccionado][mes_seleccionado].name + ' - ' + months[0][year_seleccionado][mes_seleccionado].data_year
+
+      //Empezar a dibujar
+
+      //Si el mes inicia desde el indice 5 en adelante creara una nueva semana
+      if (months[0][year_seleccionado][mes_seleccionado].start_position >= 5) {
+        //fw is from 'five week
+        const fw = document.createElement('tr')
+        fw.classList.add('calendary__week-row')
+        fw.classList.add('fw')
+
+        for (let i = 0; i < 7; i++) {
+          const fw_day = document.createElement('td')
+          fw_day.classList.add('calendary__date')
+          fw_day.classList.add('fw_day')
+          fw_day.textContent = "";
+
+          fw.appendChild(fw_day)
+        }
+
+        //Meter el five week en html
+        tbody.appendChild(fw);
+
+        //Creo que tiene sentido, cada vez se actualizan
+        day = document.querySelectorAll(".calendary__date");
+        table_data = tbody.querySelectorAll("td");
+        table_rows = tbody.querySelectorAll('tr')
+
+
+
+      }
+      let contador = 1;
+      for (let i = months[0][year_seleccionado][mes_seleccionado].start_position; i < months[0][year_seleccionado][mes_seleccionado].days; i++) {
+
+        table_data[i].textContent = contador
+        contador += 1
+
+        let index = i - months[0][year_seleccionado][mes_seleccionado].start_position + 1;
+
+        addel[i] = () => {
+          mostrarmodal(index)
+        }
+        day[i].addEventListener('click', addel[i])
+      }
+
+      for (let i = 0; i < day.length; i++) {
+        if (day[i].textContent == "") {
+          day[i].classList.add('calendary__date--previous-month')
+        }
+
+      }
+    }
+  })
+}
+
+//Solo cuando se carga la Pagina
+calendary_header.textContent = months[0][year_seleccionado][mes_seleccionado].name + ' - ' + months[0][year_seleccionado][mes_seleccionado].data_year
+
+for (let i = 0; i < table_data.length; i++) {
+  table_data[i].textContent = "";
+  day[i].removeEventListener("click", addel[i])
+  day[i].classList.remove('calendary__date--previous-month')
+}
+
+if (months[0][year_seleccionado][mes_seleccionado].start_position >= 5) {
+  //fw is from 'five week
+  const fw = document.createElement('tr')
+  fw.classList.add('calendary__week-row')
+  fw.classList.add('fw')
+
+  for (let i = 0; i < 7; i++) {
+    const fw_day = document.createElement('td')
+    fw_day.classList.add('calendary__date')
+    fw_day.classList.add('fw_day')
+    fw_day.textContent = "";
+
+    fw.appendChild(fw_day)
+  }
+
+  //Meter el five week en html
+  tbody.appendChild(fw);
+
+  //Creo que tiene sentido, cada vez se actualizan
+  day = document.querySelectorAll(".calendary__date");
+  table_data = tbody.querySelectorAll("td");
+  table_rows = tbody.querySelectorAll('tr')
 }
 
 let counter = 1;
-for (let i = months[mes_seleccionado].start_position; i < months[mes_seleccionado].days; i++) {
+for (let i = months[0][year_seleccionado][mes_seleccionado].start_position; i < months[0][year_seleccionado][mes_seleccionado].days; i++) {
 
 
-    table_data[i].textContent = counter;
-    counter += 1;
+  table_data[i].textContent = counter;
+  counter += 1;
 
-    let index = i - months[mes_seleccionado].start_position + 1;
+  let index = i - months[0][year_seleccionado][mes_seleccionado].start_position + 1;
 
-    addel[i] = () => {
-        mostrarmodal(index)
-    }
-    day[i].addEventListener('click', addel[i])
+  addel[i] = () => {
+    mostrarmodal(index)
+  }
+  day[i].addEventListener('click', addel[i])
 
 
 }
 
 //Asigna previous month en la primera carga
 for (let i = 0; i < day.length; i++) {
-    console.log("Fix" + day[i].textContent)
-    if (day[i].textContent === "") {
-        day[i].classList.add('calendary__date--previous-month')
-    }
+  if (day[i].textContent === "") {
+    day[i].classList.add('calendary__date--previous-month')
+  }
 }
 
 
@@ -186,25 +288,25 @@ for (let i = 0; i < day.length; i++) {
 
 //El formulario es enviado y dentro de un array se crea un evento
 formulario.addEventListener('submit', (form) => {
-    form.preventDefault();
-    let evento = {
-        title: document.querySelector('#event__title').value,
-        day: document.querySelector('#daysel').value,
-        month: document.querySelector('#monthsel').value,
-        year: document.querySelector('#yearsel').value,
-        hour: document.querySelector('#time__date').value
-    }
+  form.preventDefault();
+  let evento = {
+    title: document.querySelector('#event__title').value,
+    day: document.querySelector('#daysel').value,
+    month: document.querySelector('#monthsel').value,
+    year: document.querySelector('#yearsel').value,
+    hour: document.querySelector('#time__date').value
+  }
 
-    eventos.push(evento)
+  eventos.push(evento)
 
-    //Usado como debug imprime los eventos existentes en el array 'eventos'
-    for (let i = 0; i < eventos.length; i++) {
-        console.log(eventos[i])
-    }
+  //Usado como debug imprime los eventos existentes en el array 'eventos'
+  for (let i = 0; i < eventos.length; i++) {
+    console.log(eventos[i])
+  }
 
-    //Resetea el formulario al terminar de crear el evento
-    formulario.reset();
+  //Resetea el formulario al terminar de crear el evento
+  formulario.reset();
 
-    //Cierra el menu
-    menu.close()
+  //Cierra el menu
+  menu.close()
 })
